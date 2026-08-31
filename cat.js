5e814c35d487fd1f57d62eadd1fc1563438e5bc6
serinefarhane-ux <serinefarhane@gmail.com>
2026-08-31 19:12:53 -0400

    Add filter functionality to buttons for item display

--- cat.js
+++ cat.js
@@ -1,1 +1,22 @@
+const buttons = document.querySelectorAll(".btn");
+const items = document.querySelectorAll(".fullRec");
 
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    // ... everything else happens inside here ...
+
+    buttons.forEach(btn => btn.classList.remove('active'));
+    
+    button.classList.add('active');
+    const filterValue = button.getAttribute("data-filter");
+    items.forEach(item => {
+        const itemGenre = item.getAttribute("data-genre");
+        if(filterValue == "all"|| filterValue == itemGenre){
+            item.classList.remove('hide'); // Show the item
+      } else {
+        item.classList.add('hide');
+        };
+    });
+  });
+});
